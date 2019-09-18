ba96e8201f3ba774f4d66759ff0515f37bced83e
Pierson Scarborough <piersonscarborough@Piersons-MacBook-Pro.local>
2019-09-17 23:05:52 -0400

    contact and resume

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+"use strict"
 //Typewrite effect
 
 var TxtType = function(el, toRotate, period) {
@@ -69,29 +69,51 @@ navBarToggle.addEventListener('click', function () {
 });
 
 
-// Get the modal
-let modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+
 let btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
 btn.onclick = function () {
+
+    let modal = document.getElementById("myModal");
+
+    let span = document.getElementsByClassName("close")[0];
+
     modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
+    span.onclick = function () {
         modal.style.display = "none";
     }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 }
 
+
+
+
+
+let submit = document.getElementById('submit')
+
+submit.onclick = function(){
+    let modal = document.getElementById("contactModal");
+    let close = document.getElementById("close");
+
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    close.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
